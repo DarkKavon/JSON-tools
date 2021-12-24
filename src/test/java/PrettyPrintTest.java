d4db2f2bb6c8e1d2c06c791c275600735d0b154e
DarkKavon <kacperwandel@gmail.com>
2021-12-24 08:45:09 +0100

    Add PrettyPrintTest

---
+++ src/test/java/PrettyPrintTest.java
@@ -0,0 +1,38 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import pl.put.poznan.transformer.logic.PrettyPrint;
+
+public class PrettyPrintTest {
+    
+    private PrettyPrint prettyPrint;
+    private ObjectMapper mapper;
+
+    @BeforeEach
+    private void setUp() {
+        mapper = new ObjectMapper();
+    }
+
+    @Test
+    public void testReturnResultOK() {
+        prettyPrint= new PrettyPrint("{\"id\":1,\"value\":\"test\"}", new String[] {});
+        String resStr = "";
+        try {
+            JsonNode resJson = mapper.readTree("{\"id\":1,\"value\":\"test\"}");
+            resStr = resJson.toPrettyString();
+        } catch(JsonMappingException e) {
+            e.printStackTrace();
+        } catch(JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        assertEquals(resStr, prettyPrint.returnResult());
+    } 
+
+}
