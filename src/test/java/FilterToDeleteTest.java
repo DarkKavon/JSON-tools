5a6f41237a3d4bab3130b51f00c55893a6c997f1
DarkKavon <kacperwandel@gmail.com>
2021-12-24 12:50:59 +0100

    Modify FilterToDeleteTest

--- src/test/java/FilterToDeleteTest.java
+++ src/test/java/FilterToDeleteTest.java
@@ -1,4 +1,4 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import com.fasterxml.jackson.databind.JsonNode;
